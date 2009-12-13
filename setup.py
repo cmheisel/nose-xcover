30107e355b334c5f0a76c78571e0a28e8d8ba865
Chris <chris@heisel.org>
2009-12-12 20:15:21 -0500

    Initial import

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='nosexcover',
+    version='0.1',
+    description='Fork of nose.plugins.cover that can output Cobertura-style XML reports',
+    long_description=open('README').read(),
+    author='Chris Heisel',
+    author_email='chris@heisel.org',
+    url='http://github.com/cmheisel/nose-xcover/',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['nose'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points = {
+        'nose.plugins': [ 'xcover = nosexcover:XCoverage' ]
+    },
+)
+
