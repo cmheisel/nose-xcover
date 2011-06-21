90eafe4fc8c4a1e1d485e88ff087a7f3aa5fce57
Chris Heisel <chris@heisel.org>
2011-06-21 14:59:59 -0400

    PEP8-ery

--- nosexcover/nosexcover.py
+++ nosexcover/nosexcover.py
@@ -1,5 +1,5 @@
 """Companion to nose.plugins.cover. Enable by adding --with-xcoverage to your
-arguments. A Cobertura-style XML file, honoring the options you pass to 
+arguments. A Cobertura-style XML file, honoring the options you pass to
 --with-coverage, will be generated in coverage.xml"""
 
 import logging
@@ -10,7 +10,8 @@ log = logging.getLogger('nose.plugins.xcover')
 
 class XCoverage(cover.Coverage):
     """
-    Add Cobertura-style XML coverage reports to the built-in nose.plugins.cover plugin.
+    Add Cobertura-style XML coverage reports
+    to the built-in nose.plugins.cover plugin.
     """
 
     def options(self, parser, env):
@@ -22,10 +23,10 @@ class XCoverage(cover.Coverage):
                           default=env.get('NOSE_XCOVER_FILE', 'coverage.xml'),
                           dest='xcoverage_file',
                           metavar="FILE",
-                          help='Path to xml file to store the coverage report in. '
+                          help='Path to xml coverage report.'
                           'Default is coverage.xml in the working directory. '
                           '[NOSE_XCOVERAGE_FILE]')
-        
+
     def configure(self, options, config):
         super(XCoverage, self).configure(options, config)
         self.xcoverageFile = options.xcoverage_file
@@ -36,9 +37,9 @@ class XCoverage(cover.Coverage):
         """
         import coverage
         coverage.stop()
-        modules = [ module
+        modules = [module
                     for name, module in sys.modules.items()
-                    if self.wantModuleCoverage(name, module) ]
+                    if self.wantModuleCoverage(name, module)]
         log.debug("Coverage report will cover modules: %s", modules)
-        morfs = [ m.__file__ for m in modules if hasattr(m, '__file__') ]
+        morfs = [m.__file__ for m in modules if hasattr(m, '__file__')]
         coverage._the_coverage.xml_report(morfs, outfile=self.xcoverageFile)
